29260dfa8b65de18cc5f3e4260f0c61128e76cc1
Tomas Korima <korimsoft@gmail.com>
2021-06-25 22:24:58 +0200

    Single frame export working

--- src/strip/exporters/single-frame-exporter.ts
+++ src/strip/exporters/single-frame-exporter.ts
@@ -1,9 +1,36 @@
+import Jimp from 'jimp/*';
 import { Exporter } from './exporter';
+import path from 'path';
 
 export class SingleFrameExporter implements Exporter {
-    
-    
-    public export() {
 
+    public async export(header: Jimp, frames: Jimp[], footer: Jimp, constants: any, outPath: string) {
+        const pendingExports: Promise<void>[] = [];
+        frames.forEach((frame, index) => {
+            const exportPath = this.transformPath(outPath, index);
+            pendingExports.push(this.exportFrame(frame, exportPath));
+        });
+
+        await Promise.all(pendingExports);
+    }
+
+    /**
+ * Transforms the output path of a frame so that there are no name conflicts,
+ * for example /my/output/file.png => /my/ouput/file-1.png
+ * @param outPath 
+ * @param frameOrder 
+ * @returns Transformed path
+ */
+    private transformPath(outPath: string, frameOrder: number): string {
+        const dir: string = path.dirname(outPath)
+        const fileName = path.basename(outPath).split('.')[0];
+        const ext = path.extname(outPath);
+        const newFileName = `${fileName}-${frameOrder}${ext}`;
+
+        return path.resolve(dir, newFileName);
+    }
+
+    private async exportFrame(frame: Jimp, outPath: string): Promise<void> {
+        await frame.writeAsync(outPath);
     }
 }
--- src/strip/strip.ts
+++ src/strip/strip.ts
@@ -1,5 +1,6 @@
 import { StripCompositor } from './compositors/strip-compositor';
 import { Exporter } from './exporters/exporter';
+import { SingleFrameExporter } from './exporters/single-frame-exporter';
 import { StripExporter}  from './exporters/strip-exporter';
 
 export const strip = async (config, constants, resultFilePath: string): Promise<void> => {
@@ -8,8 +9,9 @@ export const strip = async (config, constants, resultFilePath: string): Promise<
         compositor = await compositor.compositeHeader(config.header);
         compositor = await compositor.compositeFooter(config.footer);
 
-
     const stripExporter: Exporter = new StripExporter();
+    const singleFrameExporter: Exporter = new SingleFrameExporter();
 
     await compositor.export(stripExporter, resultFilePath);
+    await compositor.export(singleFrameExporter, resultFilePath);
 }
