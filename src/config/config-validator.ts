1229cce0a6f3a8eff22e69ecb414ebcfb915f678
Tomas Korima <korimsoft@gmail.com>
2021-07-23 21:47:33 +0200

    Degulpification

--- src/config/config-validator.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from './model/config';
-
-export class ConfigValidator {
-    public validate(config: Config): void {
-
-    }
-}
---
+++ tests/config/strip-config-validator.spec.ts
@@ -0,0 +1,3 @@
+describe('StripConfigValidator', () => {
+
+});
